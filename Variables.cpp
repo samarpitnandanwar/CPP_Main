45b749ff3d59aaca60e12fdf243df82e7e26edb9
Samarpit <samarpit.snandanwar@gmail.com>
2024-04-09 14:42:49 +0530

    Create Variables.cpp

---
+++ Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 7;
+    float b = 9.9;
+    char c = 'a';
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+
+    return 0;
+}
